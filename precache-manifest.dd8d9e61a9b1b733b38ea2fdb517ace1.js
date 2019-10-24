46112c0746f74d799b78e541719fc027791ad455
Murodjon <tmurodon@gmail.com>
2019-10-24 12:42:17 +0100

    Updates

---
+++ precache-manifest.dd8d9e61a9b1b733b38ea2fdb517ace1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229b1d906fe4ac5e3f2cf6395ff62385",
+    "url": "/reactTodoApp/index.html"
+  },
+  {
+    "revision": "e059ab565161577999b9",
+    "url": "/reactTodoApp/static/css/main.90cf71bc.chunk.css"
+  },
+  {
+    "revision": "0f847985858f15db61b0",
+    "url": "/reactTodoApp/static/js/2.fee8b7f8.chunk.js"
+  },
+  {
+    "revision": "e059ab565161577999b9",
+    "url": "/reactTodoApp/static/js/main.1aa97e1c.chunk.js"
+  },
+  {
+    "revision": "ee5797050cf1a17bb950",
+    "url": "/reactTodoApp/static/js/runtime-main.77cba6c5.js"
+  }
+]);
